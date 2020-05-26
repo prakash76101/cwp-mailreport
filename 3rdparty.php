2a78776b60358bb569e3b478a40f26e8f9dc1aae
prakash76101 <65738388+prakash76101@users.noreply.github.com>
2020-05-26 21:23:12 +0530

    Create 3rdparty.php

---
+++ 3rdparty.php
@@ -0,0 +1,18 @@
+<script type='text/javascript'>$(document).ready(function(){$(".custom-menu").prev("LI").css("display","none");});</script>
+<!-- MAIL REPORT -->
+<noscript>
+<li class="custom-menu"> <!-- this class "custom-menu" was added so you can remove the Developer Menu easily if you want -->
+    <a href="?module=mailrelatorio"><span class="icon16 icomoon-icon-arrow-right-3"></span>MAIL REPORT</a>
+</li>
+</noscript>
+<script type="text/javascript">
+        $(document).ready(function() {
+                var newButtons = ''
+                  +' <li>'
+                  +' <a href="?module=mailrelatorio" class=""><span aria-hidden="true" class="icon16 icomoon-icon-arrow-right-3"></span>MAIL REPORT</a>'
+                  +'</li>';
+                $("ul#mn-15-sub").append(newButtons);
+        });
+</script>
+<!-- end MAILREPORT -->
+   
